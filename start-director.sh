10db28b1584963f667b3572c320d00418b5e3529
modijhalak <33565143+modijhalak@users.noreply.github.com>
2017-11-11 17:48:30 +0800

    Update start-director.sh

--- start-director.sh
+++ start-director.sh
@@ -1,4 +1,6 @@
-echo "lp.plugin.configuration.blacklist: sandbox" >> /etc/cloudera-director-server/application.properties
+wget http://archive.cloudera.com/director/redhat/7/x86_64/director/cloudera-director.repo -O /etc/yum.repos.d/cloudera-director.repo
+
+
 service cloudera-director-server start 
 until service cloudera-director-server status; do 
   echo "cloudera-director-server is not starting. Try to restart service in 5 sec...."
